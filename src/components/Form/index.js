34633e6278b35b423b6cda3332aad3078cc37080
Kirill Petrov <k.petrov@wellyes.ru>
2018-07-31 14:58:02 +0300

    Form success message

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -78,7 +78,10 @@ class Form extends Component {
         ...this.state
       })
     })
-      .then(() => {navigateTo(form.getAttribute("action"); this.props.clickHandler}))
+      .then(() => {
+        navigateTo(form.getAttribute("action"));
+        this.props.clickHandler()
+      })
       .catch(error => alert(error));
   };
 
