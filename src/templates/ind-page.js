8113dfceecdfee8b02dbc2ea3d564bfd427d743e
Kirill Petrov <k.petrov@wellyes.ru>
2018-08-29 13:03:15 +0300

    Projects fix

--- src/templates/ind-page.js
+++ src/templates/ind-page.js
@@ -65,7 +65,6 @@ export const industriesPageQuery = graphql`
           frontmatter {
             templateKey
             title
-            category
             smallDescr
             preview
             categories {
